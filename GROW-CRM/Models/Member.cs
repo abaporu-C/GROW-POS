dbf8d014162d7ac4148639f29d33a322a0e58984
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2022-02-07 19:59:13 -0500

    Add member blank error fixed and UI updated according to Dave Kendall suggestions

--- GROW-CRM/Models/Member.cs
+++ GROW-CRM/Models/Member.cs
@@ -26,10 +26,10 @@ namespace GROW_CRM.Models
         {
             get
             {
-                return FirstName[0].ToString().ToUpper() + FirstName.Substring(1)
+                return FirstName?[0].ToString().ToUpper() + FirstName?.Substring(1)
                     + (string.IsNullOrEmpty(MiddleName) ? " " :
-                        (" " + (char?)MiddleName[0] + ". ").ToUpper())
-                    + LastName[0].ToString().ToUpper() + LastName.Substring(1);
+                        (" " + (char?)MiddleName?[0] + ". ").ToUpper())
+                    + LastName?[0].ToString().ToUpper() + LastName?.Substring(1);
             }
         }
 
