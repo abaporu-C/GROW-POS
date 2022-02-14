53e99a25a526404a2337f0d61d875915fff338ce
Luis Marquez <lmarquez1@ncstudents.niagaracollege.ca>
2022-02-14 14:50:11 -0500

    Created Lookup Tabs and Document Maintenance

---
+++ GROW-CRM/Controllers/DietaryRestrictionsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GROW_CRM.Data;
+using GROW_CRM.Models;
+
+namespace GROW_CRM.Controllers
+{
+    public class DietaryRestrictionsController : Controller
+    {
+        private readonly GROWContext _context;
+
+        public DietaryRestrictionsController(GROWContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DietaryRestrictions
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Lookups", new { Tab = ControllerName() + "Tab" });
+        }
+
+        // GET: DietaryRestrictions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dietaryRestriction = await _context.DietaryRestrictions
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (dietaryRestriction == null)
+            {
+                return NotFound();
+            }
+
+            return View(dietaryRestriction);
+        }
+
+        // GET: DietaryRestrictions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DietaryRestrictions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Restriction")] DietaryRestriction dietaryRestriction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dietaryRestriction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Lookups", new { Tab = ControllerName() + "Tab" });
+            }
+            return View(dietaryRestriction);
+        }
+
+        // GET: DietaryRestrictions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dietaryRestriction = await _context.DietaryRestrictions.FindAsync(id);
+            if (dietaryRestriction == null)
+            {
+                return NotFound();
+            }
+            return View(dietaryRestriction);
+        }
+
+        // POST: DietaryRestrictions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Restriction")] DietaryRestriction dietaryRestriction)
+        {
+            if (id != dietaryRestriction.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dietaryRestriction);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DietaryRestrictionExists(dietaryRestriction.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index", "Lookups", new { Tab = ControllerName() + "Tab" });
+            }
+            return View(dietaryRestriction);
+        }
+
+        // GET: DietaryRestrictions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dietaryRestriction = await _context.DietaryRestrictions
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (dietaryRestriction == null)
+            {
+                return NotFound();
+            }
+
+            return View(dietaryRestriction);
+        }
+
+        // POST: DietaryRestrictions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dietaryRestriction = await _context.DietaryRestrictions.FindAsync(id);
+            _context.DietaryRestrictions.Remove(dietaryRestriction);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Lookups", new { Tab = ControllerName() + "Tab" });
+        }
+
+        private string ControllerName()
+        {
+            return this.ControllerContext.RouteData.Values["controller"].ToString();
+        }
+
+        private bool DietaryRestrictionExists(int id)
+        {
+            return _context.DietaryRestrictions.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ GROW-CRM/Controllers/LookupsController.cs
@@ -0,0 +1,71 @@
+﻿using GROW_CRM.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GROW_CRM.Controllers
+{
+    public class LookupsController : Controller
+    {
+        private readonly GROWContext _context;
+
+        public LookupsController(GROWContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(string Tab)
+        {
+            ///Note: select the tab you want to load by passing in
+            ///the ID of the tab such as MedicalTrialsTab, ConditionsTab
+            ///or AppointmentReasonsTab
+            ViewData["Tab"] = Tab;
+            return View();
+        }
+
+        public PartialViewResult DietaryRestrictions()
+        {
+            ViewData["DietaryRestrictionsID"] = new
+                SelectList(_context.DietaryRestrictions
+                .OrderBy(a => a.Restriction), "ID", "Restriction");
+            return PartialView("_DietaryRestrictions");
+        }
+
+        public PartialViewResult Genders()
+        {
+            ViewData["GendersID"] = new
+                SelectList(_context.Genders
+                .OrderBy(a => a.Name), "ID", "Name");
+            return PartialView("_Genders");
+        }
+
+        public PartialViewResult Provinces()
+        {
+            ViewData["ProvincesID"] = new
+                SelectList(_context.Provinces
+                .OrderBy(a => a.Name), "ID", "Name");
+            return PartialView("_Provinces");
+        }
+
+        public PartialViewResult NotificationTypes()
+        {
+            ViewData["NotificationTypesID"] = new
+                SelectList(_context.NotificationTypes
+                .OrderBy(a => a.Type), "ID", "Type");
+            return PartialView("_NotificationTypes");
+        }
+
+        public PartialViewResult PaymentTypes()
+        {
+            ViewData["PaymentTypesID"] = new
+                SelectList(_context.PaymentTypes
+                .OrderBy(a => a.Type), "ID", "Type");
+            return PartialView("_PaymentTypes");
+        }
+
+
+    }
+}
