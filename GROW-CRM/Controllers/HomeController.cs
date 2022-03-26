4d5590d64095d9de00bb9543c2e8552aa13840a2
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2022-03-26 01:14:09 -0500

    NavBar Visuals To Green And Right Anchors

--- GROW-CRM/Controllers/HomeController.cs
+++ GROW-CRM/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using GROW_CRM.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace GROW_CRM.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
