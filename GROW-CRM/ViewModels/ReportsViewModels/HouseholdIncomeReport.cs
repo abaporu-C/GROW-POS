19a7fc2da8dacc37accf805fb85afc6abe96fd10
Hare-J <jhare5@niagaracollege.ncstudents.ca>
2022-04-01 23:43:11 -0400

    Fixed formatting on all downloadable Excel reports.

---
+++ GROW-CRM/ViewModels/ReportsViewModels/HouseholdIncomeReport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using GROW_CRM.Models;
+
+namespace GROW_CRM.ViewModels.ReportsViewModels
+{
+    public class HouseholdIncomeReport
+    {
+        public int ID { get; set; }
+        public string Household { get; set; }
+
+        [Display(Name = "Total Income")]
+        public decimal Income { get; set; }
+
+        [Display(Name = "Income Situation")]
+        public string Income_Situation { get; set; }
+
+        [Display(Name = "Number of Members")]
+        public int Number_of_Members { get; set; }
+    }
+}
