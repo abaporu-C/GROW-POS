d23bdffc5be2bca2a0cd982973389959a010b6fb
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2022-04-02 22:34:49 -0500

    Added Download Raw Data Functionality to Reports

---
+++ GROW-CRM/ViewModels/ReportsViewModels/ExcelColumnFormating.cs
@@ -0,0 +1,14 @@
+﻿namespace GROW_CRM.ViewModels.ReportsViewModels
+{
+    public class ExcelColumnFormating
+    {
+        public ExcelColumnFormating()
+        {
+
+        }
+
+        public int Column { get; set; }
+
+        public string Format { get; set; }
+    }
+}
