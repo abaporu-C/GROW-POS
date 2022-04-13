9e8535d7e323571bc1feed656af37f7d876964a6
Hare-J <jhare5@niagaracollege.ncstudents.ca>
2022-04-13 01:17:09 -0400

    Fixed issue where date-descending sort didn't apply when clicking "Back to Orders" buttons.

--- GROW-CRM/Controllers/OrdersController.cs
+++ GROW-CRM/Controllers/OrdersController.cs
@@ -41,6 +41,8 @@ namespace GROW_CRM.Controllers
                                .Include(o => o.PaymentType)
                                .AsNoTracking()
                          select o;
+
+            
             //Add as many filters as needed
             if (HouseholdID.HasValue)
             {
@@ -165,6 +167,11 @@ namespace GROW_CRM.Controllers
                      .ThenByDescending(h => h.Member.LastName)
                      .ThenByDescending(o => o.Member.FirstName);
                 }
+            }
+            else
+            {
+                sortField = "Date";
+                sortDirection = "desc";
             }
 
 
